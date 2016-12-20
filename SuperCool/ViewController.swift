5ed07da7a6fdea70f9ed93ab7c22016f7c2ed9b8
Elizabeth Cowan <ecowan@ucsd.edu>
2016-12-20 14:39:12 -0800

    added files for supercool practice app

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Lizzy Cowan on 12/20/16.
+//  Copyright © 2016 lizzytest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var unCoolButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func makeMeNotSoUncool(_ sender: Any) {
+        coolLogo.isHidden = false
+        coolBg.isHidden = false
+        unCoolButton.isHidden = true
+    }
+
+}
+
